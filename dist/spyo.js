c1866017c07e79596d12ba7f5a7f8eb1f1d4bce8
fabioricali <ricalifabio@gmail.com>
2018-02-07 18:21:37 +0100

    v0.0.2

--- dist/spyo.js
+++ dist/spyo.js
@@ -1,4 +1,4 @@
-// [AIV]  Spyo Build version: 0.0.1  
+// [AIV]  Spyo Build version: 0.0.2  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
